d6c5a10a301d02cef8be86622a664ea8aa4d3180
Ohguk Kwon <ohguk.kwon@gmail.com>
2023-05-22 19:56:15 -0700

    5/22

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include <Arduino.h>
 #include <time.h>
+#include "max6675.h"
 
 #define red_led 9
 #define green_led 10
 #define blue_led 11
 #define LED_biult 13
 
-#define do_LED_OUT1 5
-#define do_LED_OUT2 6
-#define do_LED_OUT3 7
+#define do_LED_OUT1 7
+#define do_LED_OUT2 8
+#define do_LED_OUT3 9
 
 #define di_1 16  //
 #define di_2 17
 #define di_3 18
 #define di_4 19
 
+#define AI_01 14
+#define AI_02 15
 
 //declare variables for write_leds function of Serial communication
 char led_specifier;
@@ -31,7 +34,24 @@ void led_builtin();
 void do_led_out1();
 void do_led_out2();
 
-char di_2_state;
+void sendData();
+int dataSet[10] = {0,0,0,0,0,0,0,0,0,0};
+int* pdataSet =dataSet;
+
+int thermoDO = 4;
+int thermoCS = 5;
+int thermoCLK = 6;
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+// void temperature1();
+void temperature1()
+{
+  // Serial.print("C = "); 
+  dataSet[2] = thermocouple.readCelsius();
+//   Serial.println(thermocouple.readCelsius());
+//   Serial.print("F = ");
+//   Serial.println(thermocouple.readFahrenheit());
+}
 
 void setup()
 {
@@ -50,58 +70,66 @@ void setup()
   analogWrite(green_led, 10);  // set the brightness of the red LED 0-255
   analogWrite(blue_led, 10);  // set the brightness of the red LED 0-255
   Serial.begin(9600);
-  Serial.flush();
+  //Serial.flush();
+
+
 }
 
 void loop()
 {
-  while (true) {
+  sendData();
 
-    do_led_out1();
-    do_led_out2();
+  do_led_out1();
+  do_led_out2();
 
-    led_builtin();
+  led_builtin();
 
-    
+  temperature1();
+  
+  led_specifier = Serial.read();
+  led_brightness = Serial.parseInt();
+  // write_leds(led_specifier, led_brightness);
+
+
+
+  if (Serial.available() >0 ){
     led_specifier = Serial.read();
     led_brightness = Serial.parseInt();
-    // write_leds(led_specifier, led_brightness);
-
-    if (Serial.available() ){
-      led_specifier = Serial.read();
-      led_brightness = Serial.parseInt();
-      led_rgb(led_specifier, led_brightness);
-      Serial.println("OK");
-    }
-    delay(100);
+    led_rgb(led_specifier, led_brightness);
+    // Serial.println("OK");
   }
-}
+  // Serial.println("");
+  delay(2);
+
+
+  }
 
 void do_led_out1()
 {
   if (digitalRead(di_1) == HIGH){
     digitalWrite(do_LED_OUT1, HIGH);
-    Serial.println("Input1 is HIGH");    
+    pdataSet[0] = 1;
+    // Serial.println("Input1 is HIGH");    
   }
   else{
     digitalWrite(do_LED_OUT1, LOW);
-    Serial.println("Input1 is Low");
+    pdataSet[0] = 0;
+    //Serial.println("Input1 is Low");
   }
   return;
 }
 
 void do_led_out2()
 {
-  di_2_state = digitalRead(di_2);
-  if (di_2_state == HIGH){
+  if (digitalRead(di_2) == HIGH){
     digitalWrite(do_LED_OUT2, HIGH);
-    // Serial.write("Input02 is HIGH");
-    // Serial.println("Input02 is HIGH");
-  }
+    pdataSet[1] = 1;
+    //Serial.println("Input2 is HIGH"); 
+  }   
   else{
-    digitalWrite(do_LED_OUT2, HIGH);
-    // Serial.write("Input02 is LOW");
-    // Serial.println("Input02 is LOW");
+    digitalWrite(do_LED_OUT2, LOW);
+    pdataSet[1] = 0;
+    //Serial.println("Input2 is Low");
   }
   return;
 }
@@ -123,8 +151,6 @@ void led_rgb(char led, int brightness)
   delay(1);
 }
 
-
-
 void led_builtin()
 {
     int length = Serial.readBytesUntil('\n', buffer, sizeof(buffer));
@@ -141,4 +167,17 @@ void led_builtin()
 bool isEqual(const char* cmd)
 {
     return strcmp(buffer, cmd) == 0;
+}
+
+void sendData()
+{
+  //create the dataSet array write to Serial
+  // if (Serial.available() >0 ){
+  Serial.print("dataSet: ");
+  int length = sizeof(dataSet)/sizeof(dataSet[0]);
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print(dataSet[i]);
+    Serial.print(" ");
+  }
 }
